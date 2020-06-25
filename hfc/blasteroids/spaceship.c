4ee0cba7c23821da3082e5a6a0677c7bd8966b4b
slanjo <slaanjoo@gmail.com>
2020-06-26 06:32:40 +0800

    work on spaceship.c

--- hfc/blasteroids/spaceship.c
+++ hfc/blasteroids/spaceship.c
@@ -1,1 +1,27 @@
 //This code is used to manage a spaceship
+#include <allegro5/allegro.h>
+
+typedef struct {
+    float sx;
+    float sy;
+    float heading;
+    float speed;
+    int gone;
+    ALLEGRO_COLOR color;
+} Spaceship;
+void draw_ship(Spaceship * s);
+
+int main(){
+
+
+
+}
+
+
+    
+    
+    
+void draw_ship(Spaceship * s){
+    ALLEGRO_TRANSFORM transform;
+
+}
