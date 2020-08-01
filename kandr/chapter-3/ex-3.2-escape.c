71c53ad1d1bb05fdd339669d4006c488bd52b8f5
slanjo <slaanjoo@gmail.com>
2020-08-01 21:33:21 +0800

    Sat 01 Aug 2020 09:33:26 PM AWST

--- kandr/chapter-3/ex-3.2-escape.c
+++ kandr/chapter-3/ex-3.2-escape.c
@@ -1,7 +1,6 @@
 //Fri 31 Jul 2020 05:30:16 AM AWST
 //Write a function escape(s, t) that converts newlines and tabs into visible escape 
 //sequence like \n and \t. Use a switch. Write a function for the other direction epacse(s, t)
-//
 #include <stdio.h>
 #define ARR_SIZE 100
 void escape(char s[], char t[]);
@@ -19,15 +18,16 @@ int main(){
     }
     s[k+1] = '\0'; 
 //    scanf("%s", u);
-    printf("string s is: %s\n", s);
-   
+//    printf("string s is: %s\n", s);
     escape(s, t);
-    epacse(s, char t[]);
+    printf("string s after ESCAPE loop: %s\n", s);
+    for (k = 0; k < 100; k++)
+        t[k] = 0;
+    epacse(s, t);
     return 0;
 }
-
 void escape(char s[], char t[]){
-    printf("string s is: %s\n", s);
+//    printf("string s is: %s\n", s);
     int i, j;
     j = 0; 
     for (i = 0; i < 100  ; i++){
@@ -49,31 +49,34 @@ void escape(char s[], char t[]){
                 break;
         }
     }
-    printf("string t is: %s\n", t); 
-
+    printf("string t in escape is: %s\n", t); 
 }
 void epacse(char s[], char t[]){
     int i, j;
+    j = 0;
     for (i = 0; i < 100; i++){
-        switch (s[i]) {
-            case '\\':
-                if (s[i+1] == 't')
-                        t[i] = "\n";
+        if (s[i] == '\\'){
+        switch (s[i+1]) {
+            case 't':
+                t[j] = '\t';
+                i++;
+                j++;
                 break;
-            
-            case '\t':{
-                t[j] = '\\';
-                t[j+1] = 't';
-                j+=2;
+            case 'n':{
+                t[j] = '\n';
+                i++;
+                j++;
                 break;      }
             default: 
                 t[j] = s[i];
                 j++;     
                 break;
+            }
         }
-
+        else{
+            t[j] = s[i];
+            j++;
+        }
     }
-
-
-
+    printf("string t epacse is: %s\n", t);
 }
