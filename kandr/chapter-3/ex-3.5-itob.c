6d6d4ac9984728c72eafc07f9fdcf58bd3ee356c
slanjo <slaanjoo@gmail.com>
2020-08-06 07:36:34 +0800

    Thu 06 Aug 2020 07:36:45 AM AWST

--- kandr/chapter-3/ex-3.5-itob.c
+++ kandr/chapter-3/ex-3.5-itob.c
@@ -1,33 +1,44 @@
-//Wed 05 Aug 2020 05:35:14 AM AWST
-//modify itoa from page 64 to print n = -2^(wordsize-1)(i.e. the 'largest'
-//negative number correctly
-//used help from https://clc-wiki.net/wiki/K%26R2_solutions:Chapter_3:Exercise_4
+//Thu 06 Aug 2020 05:24:05 AM AWST
+//write itob(n, s, b) to convert the integer n into a base b character representation in string s. 
+//i.e. ntob(n, s, 16) is a hexadecimal integer s. 
+//
+//
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
 #include <string.h>
 #define AR_SIZE 100
-void itoa(int n, char s[]);
+enum hexa {A = 10, B = 11, C = 12, D = 13, E = 14, F = 16}; 
+void itob(int n, char s[], int b);
 char* reverse(char s[]);
 int main(){
-    int n;
+    int n, b;
     int sign;
+    b = 16;
     printf("INT_MIN: %d\n", INT_MIN);
-    n = INT_MIN;
+//    n = INT_MIN;
+//    n = 256;
+    n = 256;
     char s[AR_SIZE];
-    itoa(n, s);
+    itob(n, s, b);
     return 0;
 }
-void itoa(int n, char s[]){
+void itob(int n, char s[], int b){
     int i, sign;
-    unsigned int n2;
+    unsigned int n2, n3;
+    n3 = (A || B || C || D || E || F);
     sign = n;
     if ((sign = n) < 0) //record the sign
         n2 = -n;
     i = 0;
     do {     //generate digits in reverse order
-        s[i++] = (n2 % 10) + '0';  //get next digit 
-    } while ((n2 /= 10) > 0);  //while (n /= 10); //w   //delete it
+        if ((n3 = n2 % b) == (A || B || C || D || E || F)){
+            s[i++] = n3 + '0';  //get next digit 
+            printf("Code is: %i\n", n2 % b );
+        }
+        else
+            s[i++] = (n3 = (n2 % b)) + '0';
+    } while ((n2 /= b) > 0);  //while (n /= 10); //w   //delete it
     if (sign < 0)
         s[i++] = '-';
     s[i] = '\0';
