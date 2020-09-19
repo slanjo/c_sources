bf9d9f626aee851c991b65ade88072822f4257c7
slanjo <slaanjoo@gmail.com>
2020-09-19 13:49:31 +0800

    Sat 19 Sep 2020 01:49:37 PM AWST

--- kandr/chapter-4/4.12-recursion.c
+++ kandr/chapter-4/4.12-recursion.c
@@ -3,30 +3,26 @@
 /* printd: print n in decimal */
 char s[10];
 int i = 0;
-char itoai(int n, int i, char s[]);
+void itoai(int n);
 
 
 int main(){
    int x = -123456;
-   itoai(x, i, s);
-//   printf("string s = %s\n", itoai(x, i, s));
+   int i = 0;
+   
+   itoai(x);
+   printf("%s", s);
 }
 
-char itoai(int n, int i, char s[])
+void itoai(int n)
 {
     if (n < 0) {
         putchar('-');
         n = -n;
     }
-    i++;
-//    char s[10];   
-/*if (n / 10)
-    printd(n / 10);
-putchar(n % 10 + '0');
-*/
     if (n / 10) {
-        s[i] = itoai((n % 10 + '0'), i, s);
+        itoai(n/10);
     }
-    return s[i] = n % 10 + '0';
-    //printf("\n" ); 
+    s[i++] = n % 10 + '0';
+    s[i] = '\0'; 
 }
