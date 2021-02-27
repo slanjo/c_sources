62f3aa6266525fe9980360baeab2fd1e539edaa2
slanjo <slaanjoo@gmail.com>
2021-02-28 00:20:47 +0800

    reverse a text file

--- dta_str/rvrs-txt-file.c
+++ dta_str/rvrs-txt-file.c
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 
 typedef struct {
-    int *stack_item; //pointer to array of stack values for dynamic allocation by malloc
+    char *stack_item; //pointer to array of stack values for dynamic allocation by malloc
     int top;
     int size;
 }Stack;
@@ -13,27 +13,32 @@ typedef struct {
 void printBinary(unsigned int n);
 void stackInitMalloc(Stack *, int );
 void freeMallocMem(Stack *);
-void push(Stack *, int );
-int pop(Stack *);
+void push(Stack *, char);
+char pop(Stack *);
 int getSize(Stack *);
 int isOverflow(Stack *);
 int isUnderflow(Stack *);
+int reverseIt(char[], char[]);
 
 int main(){
 
-    printBinary(128); 
-
+//    printBinary(128); 
+    int f = reverseIt("/home/slanjo/Programming/C/dta_str/input_text.txt", "/home/slanjo/Programming/C/dta_str/output_text.txt");
+    if (f)
+        printf("File copied successfully\n");
+    else
+        printf("Error -- copy failed\n");
     return 0;
 }
 
-void push(Stack *s, int value){
+void push(Stack *s, char value){
     
 //    if ( s->top == s->size - 1 ){
 //        printf("Stack Overflow \n");
 //        return;
         if (isOverflow(s)){
-        int *temp;
-        temp = (int *) malloc(sizeof(int) * s->size * 2);
+        char *temp;
+        temp = (char *) malloc(sizeof(char) * s->size * 2);
         if ( temp == NULL ){
             printf("Stack Overflow\n");
             return;
@@ -50,12 +55,12 @@ void push(Stack *s, int value){
     s->stack_item[s->top] = value;
 } 
 
-int pop(Stack *s ){
+char pop(Stack *s ){
 
 //     if (s->top == -1){
     if (isUnderflow(s)){
         printf("Stack Underflow\n");
-        return -9999;
+        return '\0';
         }
     int v = s->stack_item[s->top];
     s->top--; 
@@ -64,7 +69,7 @@ int pop(Stack *s ){
 void stackInitMalloc(Stack *s, int size){
     
     s->top = -1;
-    s->stack_item = (int *) malloc(sizeof(int) * size);
+    s->stack_item = (char *) malloc(sizeof(char) * size);
     if ( s->stack_item == NULL  ){
         printf("Failed to alocate memory\n");
         exit(1);
@@ -115,3 +120,41 @@ int isOverflow(Stack *s){
 int isUnderflow(Stack *s){
     return s->top == -1;
 }
+
+int reverseIt(char source[], char dest[]){
+    printf("in reverseIt\n");
+    int i = 0;
+    while(!'\0') {
+        printf("%c", source[i]);
+        i++;
+
+                }
+    FILE *fps, *fpd;
+    const int SIZE = 50;
+    fps = fopen(source, "r");
+    if(fps = NULL){
+        printf("Error opening the file %s\n", source);
+        return 0;
+    }
+    fpd = fopen(dest, "r");
+    if(fpd == NULL){
+        printf("Errof opening the file %s\n", dest);
+        return 0;
+            }
+    Stack s;
+    stackInitMalloc(&s, SIZE);
+    char buff;
+    buff = fgetc(fps);
+    while(!feof(fps)){
+        push(&s, buff);
+        buff = fgetc(fps);
+    }
+    while(!isUnderflow(&s)){
+        fputc(pop(&s), fpd);
+    }
+    fclose(fps);
+    fclose(fpd);
+    freeMallocMem(&s);
+    return 1;
+}
+            
