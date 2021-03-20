918f0e9ce81eef67b1d085b2be302676f20633bf
slanjo <slaanjoo@gmail.com>
2021-03-20 19:13:58 +0800

    commit sort n-lines-in a file

---
+++ kandr/chapter-5/cmpr-readlines.c
@@ -0,0 +1,32 @@
+//Sat Mar 20 19:02:20 AWST 2021
+#define MAXLEN 1000    /* max length of any input line */
+char *alloc(int);
+
+/*readlines: read input lines */
+
+int readlines(char #lineptr[], int maxlines){
+    int len, nlines;
+    char *p, line[MAXLEN];
+
+    nlines = 0; 
+    while ((len = getlinee(line, MAXLEN)) > 0){
+        if (nlines >= maxlines || (p = alloc(len)) == NULL )
+            return -1;
+        else {
+            line[len - 1] = '\0'; /* delete newline */ 
+            strcpy(p, line);
+            lineptr[nlines++] = p;
+        }
+    return nlines;
+    }
+}
+/* writelines: write output lines */
+
+void writelines(char *lineptr[], int nlines){
+    int i;
+
+//    for (i = 0; i < nlines; i++)
+//        printf("%s\n", lineptr[i]);
+    while(nlines-- > 0)
+        printf ("%s\n", *lineptr++)
+}
--- kandr/chapter-5/cmpr-strings.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <string.h> 
-
-#define MAXLINES 5000
-
-char *lineptr[MAXLINES];
-
-int readlines(char *lineptr[], int nlines);
