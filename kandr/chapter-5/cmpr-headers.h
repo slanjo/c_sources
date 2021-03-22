f7e75d383943ce3f2c3655586cc233430f403318
slanjo <slaanjoo@gmail.com>
2021-03-22 20:39:06 +0800

    compare strings - Mon 22 Mar 2021 08:39:15 PM AWST

--- kandr/chapter-5/cmpr-headers.h
+++ kandr/chapter-5/cmpr-headers.h
@@ -1,5 +1,10 @@
 //Sat 20 Mar 2021 09:47:01 PM AWST
+#include <stdio.h>
+#include <string.h>
 int readlines(char *lineptr[], int nlines);
-void writelines(char *lienptr[], int nlines);
-void qsorts(char *lienptr[], int left, int right);
+void writelines(char *lineptr[], int nlines);
+void qsorts(char *lineptr[], int left, int right);
 int getlinee(char s[], int lim);
+void swap(char *v[], int i, int j);
+char *alloc(int n);
+//int strcmp(char *s, char *t);
