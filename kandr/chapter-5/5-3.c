00c4343bf37b3a29b396b0adfe09a0e5b8da6d98
slanjo <slaanjoo@gmail.com>
2021-05-15 23:06:04 +0800

    commit-branch-exerciese-5-3-strcat-pointers-

--- kandr/chapter-5/5-3.c
+++ kandr/chapter-5/5-3.c
@@ -1,8 +1,12 @@
+//Redo a chapter two function with pointers
 #include <stdio.h>
 void strcat(char *, char *);
 void strcat(char *, char *);
-int main(){
 
+int main(){
+    char *A = "Starting and then";
+    char *B = "continuing";
+    printf("%s", pstrat(&A, &B));
 }
 
 void strcat( char s[], char t[]){
@@ -14,12 +18,12 @@ void strcat( char s[], char t[]){
     while ((s[i++] = t[j++] != '\0'))
         ;
 }
-void pstrcat( char s[], char t[]){
+void pstrcat( char *s, char *t){
     int i, j;
 
     i = j = 0;
-    while (s[i] != '\0') /* find end of s*/
-        i++;
-    while ((s[i++] = t[j++] != '\0'))
+    while ( *s != '\0') /* find end of s*/
+        s++;
+    while ((*s = *t != '\0'))
         ;
 }
