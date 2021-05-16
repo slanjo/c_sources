4d204e7f1632611ea54600d5b8933e9d4b2e9243
slanjo <slaanjoo@gmail.com>
2021-05-16 11:25:12 +0800

    Sun 16 May 2021 11:25:23 AM AWST

--- kandr/chapter-5/5-3.c
+++ kandr/chapter-5/5-3.c
@@ -1,30 +1,32 @@
 //Redo a chapter two function with pointers
 //Sat May 15 2021
 #include <stdio.h>
-void strcat(char *, char *);
-void strcat(char *, char *);
+
+void strcata(char [], char []);
+void pstrcata(char [] , char []);
 
 int main(){
-    char *A = "Starting and then";
-    char *B = "continuing";
-    printf("%s", pstrat(&A, &B));
+    char A[] = "Blue";
+    char B[] = "Black";
+    pstrcata(A, B);
+    printf("%s\n", A);
 }
-
-void strcat( char s[], char t[]){
+/*
+void strcata( char s[], char t[]){
     int i, j;
 
-    i = j = 0;
-    while (s[i] != '\0') /* find end of s*/
+    int i = j = 0;
+    while (s[i] != '\0') // find end of s
         i++;
     while ((s[i++] = t[j++] != '\0'))
         ;
-}
-void pstrcat( char *s, char *t){
-    int i, j;
-
-    i = j = 0;
-    while ( *s != '\0') /* find end of s*/
-        s++;
-    while ((*s = *t != '\0'))
+}*/
+void pstrcata( char s[], char t[]){
+    printf("s = %c, t = %c\n", *s, *t);
+    while ( *s != '\0' ){ /* find end of s*/
+       s++;
+    }
+    printf("%s\n", *s);
+    while ((*s++ = *t++))
         ;
 }
